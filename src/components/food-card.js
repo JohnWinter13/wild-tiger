f6f8e683c5c080f80886c8a1f93ac88cf4d4b421
Youssef Rizkalla <32617102+JohnWinter13@users.noreply.github.com>
2019-03-06 20:59:34 -0500

    Add food prices

--- src/components/food-card.js
+++ src/components/food-card.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
-import Img from "gatsby-image"
+import Img from 'gatsby-image'
+
+import { FaMoneyBillWave } from 'react-icons/fa'
 
 const Card = styled.div`
   box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
@@ -14,12 +16,14 @@ const Card = styled.div`
   }
 `
 
-const FoodCard = ({title, desc, img}) => (
+const FoodCard = ({title, desc, img, price}) => (
   <Card>
     {img && <Img sizes={img} alt="Food" style={{width: '100%', height: '200px', borderRadius: '5px 5px 0 0'}}/>}
-    <div style={{padding: '2px 16px'}}>
-      <h4><b>{title}</b></h4>
-      <p>{desc}</p>
+    <div style={{padding: '10px'}}>
+      <h4 style={{display: 'inline'}}><b>{title}</b></h4>
+      <h5 style={{display: 'inline', color: 'green', float: 'right'}}><b>{price}</b></h5>
+      {price && <FaMoneyBillWave style={{color: 'green', display: 'inline', float: 'right', margin: '1px 3px'}}/>}
+      <p style={{fontSize: '12px', lineHeight: '15px', marginTop: '5%'}}>{desc}</p>
     </div>
   </Card>
 )
--- src/components/menu-section.js
+++ src/components/menu-section.js
@@ -47,12 +47,12 @@ const Category = ({category, foodImgData}) => (
           const imgData = foodImgData.find(img => img.node.relativePath === foodItem.img)
           if (imgData) {
             return (
-              <FoodCard title={foodItem.name} desc={foodItem.description} img={imgData.node.childImageSharp.sizes} />
+              <FoodCard title={foodItem.name} desc={foodItem.description} price={foodItem.price} img={imgData.node.childImageSharp.sizes} />
             )
           }
           else {
             return (
-              <FoodCard title={foodItem.name} desc={foodItem.description}/>
+              <FoodCard title={foodItem.name} desc={foodItem.description} price={foodItem.price}/>
             )
           }
         })
