ded5126456b1d1ce2646b0b1ae9ad0f02dc5f4ae
Youssef Rizkalla <32617102+JohnWinter13@users.noreply.github.com>
2019-03-10 15:16:49 -0400

    Update reservations page

--- src/pages/reservations.js
+++ src/pages/reservations.js
@@ -1,16 +1,43 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
+
+import { FaPhone } from 'react-icons/fa'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import GMap from '../components/gmap'
+import ReserveGrid from '../components/reserve-grid'
+import OpenTimes from '../components/open-times'
+import FoodToGo from '../components/food-to-go'
 
-const ReservationsPage = () => (
+const ReservationsPage = ({data}) => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <SEO title="Reservations"/>
+    <div style={{width: '100%', minHeight: '100vh', marginTop: '50px'}}>
+      <Img style={{position: "absolute", left: 0, top: 0, height: "100%", width: "100%", zIndex: "-1"}} sizes={data.file.childImageSharp.sizes}/>
+      <div style={{textAlign: 'center', color: '#FFF', padding: '5% 0',}}>
+        <h1 style={{fontFamily: 'Pacifico'}}>Reservations</h1>
+        <h3>For reservations, please call us during operating hours</h3>
+        <FaPhone size={26}/>
+        <h3 style={{display: 'inline'}}>+1 360-882-8887</h3>
+      </div>
+      <ReserveGrid/>
+      <GMap/>
+    </div>
   </Layout>
 )
+
+export const imgQuery = graphql`
+  query {
+    file(relativePath: { eq: "relax.jpg" }) { 
+      childImageSharp {
+        sizes(quality: 90) {
+          ...GatsbyImageSharpSizes
+        }
+      }
+    }
+  }
+`
 
 export default ReservationsPage
